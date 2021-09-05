daf1298cce2f0af5788684a13c7abcae8069b1b8
Zlatko Čalušić <zcalusic@bitsync.net>
2021-09-05 10:07:23 +0200

    Add moosefs image

---
+++ moosefs/README.md
@@ -0,0 +1,39 @@
+# Run MooseFS distributed file system in a Docker container
+
+[![Pulls](https://img.shields.io/docker/pulls/zcalusic/moosefs.svg)](https://hub.docker.com/r/zcalusic/moosefs/)
+[![Stars](https://img.shields.io/docker/stars/zcalusic/moosefs.svg)](https://hub.docker.com/r/zcalusic/moosefs/)
+
+Run [MooseFS](https://moosefs.com/) distributed file system in a Docker container.
+
+## Usage
+
+Pull image:
+
+```
+sudo docker pull zcalusic/moosefs
+```
+
+Run container:
+
+```
+sudo docker run -d \
+     -p 94xx:94xx \
+     -v your-moosefs-config:/etc/mfs \
+     -v your-moosefs-data:/var/lib/mfs \
+     -e MOOSEFS_START=master \
+     -e TZ=Europe/Zagreb \
+     --name moosefs \
+     zcalusic/moosefs
+```
+
+## Building container image
+
+Build image:
+
+```
+sudo make docker_build
+```
+
+## Instructions
+
+`MOOSEFS_START` should be one of: master, metalogger, chunkserver, client or cgi.
---
+++ moosefs/entrypoint.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+#
+# Copyright © 2021 Zlatko Čalušić
+#
+# Use of this source code is governed by an MIT-style license that can be found in the LICENSE file.
+#
+
+set -e
+
+case $MOOSEFS_START in
+    master)
+	[ -d "/etc/mfs" ] || exit 1
+	[ -f "/etc/mfs/mfsmaster.cfg" ] || cp -a /etc/mfs.default/mfsmaster.cfg.sample /etc/mfs/mfsmaster.cfg
+	[ -f "/etc/mfs/mfsexports.cfg" ] || cp -a /etc/mfs.default/mfsexports.cfg.sample /etc/mfs/mfsexports.cfg
+	[ -f "/etc/mfs/mfstopology.cfg" ] || cp -a /etc/mfs.default/mfstopology.cfg.sample /etc/mfs/mfstopology.cfg
+
+	[ -d "/var/lib/mfs" ] || exit 1
+	[ -f "/var/lib/mfs/metadata.mfs.empty" ] || cp -a /var/lib/mfs.default/metadata.mfs.empty /var/lib/mfs/metadata.mfs.empty
+	[ -f "/var/lib/mfs/metadata.mfs" ] || [ -f "/var/lib/mfs/metadata.mfs.back" ] || [ -f "/var/lib/mfs/metadata.mfs.back.1" ] || cp -a /var/lib/mfs/metadata.mfs.empty /var/lib/mfs/metadata.mfs
+	chown -R daemon:daemon /var/lib/mfs
+
+	CMD="mfsmaster -f -a"
+	;;
+    metalogger)
+	[ -d "/etc/mfs" ] || exit 1
+	[ -f "/etc/mfs/mfsmetalogger.cfg" ] || cp -a /etc/mfs.default/mfsmetalogger.cfg.sample /etc/mfs/mfsmetalogger.cfg
+
+	[ -d "/var/lib/mfs" ] || exit 1
+	chown -R daemon:daemon /var/lib/mfs
+
+	CMD="mfsmetalogger -f"
+	;;
+    chunkserver)
+	[ -d "/etc/mfs" ] || exit 1
+	[ -f "/etc/mfs/mfschunkserver.cfg" ] || cp -a /etc/mfs.default/mfschunkserver.cfg.sample /etc/mfs/mfschunkserver.cfg
+	[ -f "/etc/mfs/mfshdd.cfg" ] || cp -a /etc/mfs.default/mfshdd.cfg.sample /etc/mfs/mfshdd.cfg
+
+	[ -d "/var/lib/mfs" ] || exit 1
+	chown -R daemon:daemon /var/lib/mfs
+
+	CMD="mfschunkserver -f"
+	;;
+    client)
+	[ -d "/etc/mfs" ] || exit 1
+	[ -f "/etc/mfs/mfsmount.cfg" ] || cp -a /etc/mfs.default/mfsmount.cfg.sample /etc/mfs/mfsmount.cfg
+
+	CMD="mfsmount -f"
+	;;
+    cgi)
+	[ -d "/var/lib/mfs" ] || exit 1
+	chown -R daemon:daemon /var/lib/mfs
+
+	CMD="mfscgiserv -f"
+	;;
+    *)
+	CMD="sleep infinity"
+	;;
+esac
+
+# shellcheck disable=SC2086
+exec $CMD
