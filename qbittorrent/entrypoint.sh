73ec6e8ff2391b6ba3b26e9aa59d485297d8f794
Zlatko Čalušić <zcalusic@bitsync.net>
2022-07-18 15:35:40 +0200

    qBittorrent 4.4.3.1 / libtorrent 1.2.17
    - switch to Debian Bookworm base image, which provides:
      - gcc 11.3
      - qt 6.2
      - libssl 3.0
    - use cmake/ninja to build both library and program
    - eatmydata support (don't use unless you know what you're doing)

--- qbittorrent/entrypoint.sh
+++ qbittorrent/entrypoint.sh
@@ -9,6 +9,7 @@ set -eu
 
 : "${QBITTORRENT_UID:=1000}"
 : "${QBITTORRENT_GID:=1000}"
+: "${EATMYDATA:=no}"
 
 groupadd -g $QBITTORRENT_GID -o qbittorrent
 useradd -u $QBITTORRENT_UID -g $QBITTORRENT_GID -o -s /usr/sbin/nologin qbittorrent
@@ -20,6 +21,12 @@ then
     mkdir -p /home/qbittorrent/.config/qBittorrent
     cp /etc/qBittorrent.conf /home/qbittorrent/.config/qBittorrent/qBittorrent.conf
     chown -R qbittorrent:qbittorrent /home/qbittorrent/.config
+fi
+
+if [ "$EATMYDATA" = "yes" ]
+then
+    LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libeatmydata.so"
+    export LD_PRELOAD
 fi
 
 exec gosu qbittorrent:qbittorrent qbittorrent-nox
